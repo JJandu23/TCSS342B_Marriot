96601b823946eeb287da2a2c30680f9edb1ede2d
Jashanpreet Jandu <67085053+Jashaja18@users.noreply.github.com>
2022-05-06 11:08:16 -0700

    Update MyBinarySearchTree.java

--- src/Units/6/MyBinarySearchTree.java
+++ src/Units/6/MyBinarySearchTree.java
@@ -1,22 +1,31 @@
+import java.util.Stack;
+
 public class MyBinarySearchTree<Type extends Comparable<Type>> {
-    private Node root;
-    private int size;
-    private boolean balancing;
-    private long comparisons;
-
     class Node {
-        Type item;
-        Node left;
-        Node right;
-        int height;
+        public Type item;
+        public Node left;
+        public Node right;
+        public int height = 0;
 
-        Node(Type item) {
+        // Constructor
+        public Node(Type item) {
             this.item = item;
         }
 
+        @Override
         public String toString() {
-            return item.toString();
+            return item.toString() + ":H" + height;
         }
+    }
+
+    private Node root;
+    private int size;
+    public long comparisons;
+
+    public MyBinarySearchTree() {
+        root = null;
+        size = 0;
+        comparisons = 0;
     }
 
     public void add(Type item) {
@@ -25,14 +34,14 @@ public class MyBinarySearchTree<Type extends Comparable<Type>> {
 
     private Node add(Type item, Node subtree) {
         if (subtree == null) {
+            subtree = new Node(item);
             size++;
-            return new Node(item);
-        }
-        if (item.compareTo(subtree.item) < 0) {
+        } else if (item.compareTo(subtree.item) < 0) {
             subtree.left = add(item, subtree.left);
         } else if (item.compareTo(subtree.item) > 0) {
             subtree.right = add(item, subtree.right);
         }
+        updateHeight(subtree);
         return subtree;
     }
 
@@ -46,22 +55,41 @@ public class MyBinarySearchTree<Type extends Comparable<Type>> {
         }
         if (item.compareTo(subtree.item) < 0) {
             subtree.left = remove(item, subtree.left);
-        } else if (item.compareTo(subtree.item) > 0) {
+        }
+        else if (item.compareTo(subtree.item) > 0) {
             subtree.right = remove(item, subtree.right);
-        } else {
+        }
+        else if(item.compareTo(subtree.item) == 0){
+            size--;
+            //System.out.println("Found: " + subtree.item + ", search: " + item);
+
+            //destination has one leaves or none
             if (subtree.left == null) {
                 return subtree.right;
-            } else if (subtree.right == null) {
+            }
+            else if (subtree.right == null) {
                 return subtree.left;
             }
-            subtree.item = find((Type) subtree.right);
-            subtree.right = remove(subtree.item, subtree.right);
+
+            //Destination has 2 children
+            //Get the inorder successor of this subtree
+            Node current = subtree.right;
+            Type min = current.item;
+            while(current.left != null) {
+                min = current.left.item;
+                current = current.left;
+            }
+
+            subtree.item = min;
+
+            subtree.right = remove(min, subtree.right);
+
         }
+        updateHeight(subtree);
         return subtree;
     }
 
     public Type find(Type item) {
-        comparisons = 0;
         return find(item, root);
     }
 
@@ -74,8 +102,9 @@ public class MyBinarySearchTree<Type extends Comparable<Type>> {
             return find(item, subtree.left);
         } else if (item.compareTo(subtree.item) > 0) {
             return find(item, subtree.right);
+        } else {
+            return subtree.item;
         }
-        return subtree.item;
     }
 
     public int height() {
@@ -87,16 +116,14 @@ public class MyBinarySearchTree<Type extends Comparable<Type>> {
     }
 
     public boolean isEmpty() {
-        return size == 0;
+        return root == null;
     }
 
     private void updateHeight(Node node) {
-        if (node == null) {
-            return;
-        }
-        node.height = 1 + Math.max(height(node.left), height(node.right));
+        node.height = Math.max(height(node.left), height(node.right)) + 1;
     }
 
+    // Compute the height of a node
     private int height(Node node) {
         if (node == null) {
             return -1;
@@ -104,14 +131,42 @@ public class MyBinarySearchTree<Type extends Comparable<Type>> {
         return node.height;
     }
 
+    @Override
     public String toString() {
-        return toString(root);
-    }
+        if(root == null)
+            return "[]";
 
-    private String toString(Node root) {
-        if (root == null) {
-            return "";
+        StringBuilder sb = new StringBuilder("[");
+        Stack<Node> stack = new Stack<>();
+        Node current = root;
+
+        //O(n)
+        //keep looping as long as there are elements to discover
+        while(!stack.isEmpty() || current != null) {
+            //if subtree is not empty then keep going left and stacking
+            while(current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+
+            //current should be null here
+            //and now the first item from stack is called
+            current = stack.pop();
+
+            sb.append(current);
+
+            //Two cases:
+            //current != null -> then we discover the right subtree of current
+            //current == null -> then subtree is done, and we pop a new element
+            //in the next iteration
+            current = current.right;
+
+            //add a separator if the stack is not empty
+            //or if there is a right node for this current subtree
+            if(!stack.isEmpty() || current != null)
+                sb.append(", ");
         }
-        return toString(root.left) + root.toString() + toString(root.right);
+
+        return sb.append("]").toString();
     }
 }
