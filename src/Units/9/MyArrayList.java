7e12ecb2ed4408e811bcd5550734472872ad7c3b
Jashanpreet Jandu <67085053+Jashaja18@users.noreply.github.com>
2022-05-29 16:33:21 -0700

    Update MyArrayList.java

--- src/Units/9/MyArrayList.java
+++ src/Units/9/MyArrayList.java
@@ -1,9 +1,13 @@
 public class MyArrayList<Type extends Comparable<Type>> {
     private int capacity = 16;
-    private Object[] list = new Object[capacity];
+    private Type[] list;
     private int size = 0;
+    public long comparisons = 0;
+    private Type item;
 
-    public int comparisons = 0;
+    public MyArrayList() {
+        list = (Type[]) new Comparable[capacity];
+    }
 
     public void insert(Type item, int index) {
         if (index > size || index < 0)
@@ -14,56 +18,48 @@ public class MyArrayList<Type extends Comparable<Type>> {
 
         //loop to move the item one index forward
         //O(i) cost
-        for (int i = size; i >= index; i--) {
-            list[i + 1] = list[i];
-        }
+        if (size + 1 - index >= 0) System.arraycopy(list, index, list, index + 1, size + 1 - index);
         list[index] = item;
         size++;
     }
 
-    @SuppressWarnings("unchecked")
     public Type remove(int index) {
         if (index > size)
             return null;
-        Type element = (Type) list[index];
+        Type element = list[index];
         //Loop to move down items
         //runs in O(i)
-        for (int i = index; i < size; i++) {
-            list[i] = list[i + 1];
-        }
+        System.arraycopy(list, index + 1, list, index, size - index);
         size--;
         return element;
     }
 
-    @SuppressWarnings("unchecked")
     public boolean contains(Type item) {
         comparisons++;
         //O(n) Loop
-        for (int i = 0; i < list.length; i++) {
-            if (list[i] != null) {
+        for (Type type : list) {
+            if (type != null) {
                 comparisons++;
-                if (((Type) list[i]).compareTo(item) == 0)
+                if (type.compareTo(item) == 0)
                     return true;
             }
         }
         return false;
     }
 
-    @SuppressWarnings("unchecked")
     public int indexOf(Type item) {
         //O(n)
         for (int i = 0; i < list.length; i++) {
             if (list[i] != null)
-                if (((Type) list[i]).compareTo(item) == 0)
+                if (list[i].compareTo(item) == 0)
                     return i;
         }
         return -1;
     }
 
-    @SuppressWarnings("unchecked")
     public Type get(int index) {
         //O(1)
-        return index >= size || index < 0 ? null : (Type) list[index];
+        return index >= size || index < 0 ? null : list[index];
     }
 
     public void set(int index, Type item) {
@@ -95,20 +91,17 @@ public class MyArrayList<Type extends Comparable<Type>> {
     private void resize() {
         Object[] temp = new Object[capacity * 2];
         //O(n) loop to copy elements to bigger array
-        for (int i = 0; i < capacity; i++) {
-            temp[i] = list[i];
-        }
+        if (capacity >= 0) System.arraycopy(list, 0, temp, 0, capacity);
         capacity *= 2;
         //reassign array
-        list = temp;
+        list = (Type[]) temp;
     }
 
-    @SuppressWarnings("unchecked")
     public void sort() {
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size - 1; j++) {
-                if (((Comparable<Type>) list[i]).compareTo((Type) list[j]) < 0) {
-                    Type t1 = (Type) list[i];
+                if (list[i].compareTo(list[j]) < 0) {
+                    Type t1 = list[i];
                     list[i] = list[j];
                     list[j] = t1;
                 }
