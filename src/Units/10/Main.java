44453279eb9ed23bf4a7339872ce35579259ba90
Jashanpreet Jandu <67085053+Jashaja18@users.noreply.github.com>
2022-06-04 20:18:48 -0700

    Create Main.java

---
+++ src/Units/10/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Maze maze = new Maze(4, 4, true);
+        maze.toString();
+        Maze maze2 = new Maze(10, 10, false);
+        maze2.toString();
+    }
+}
